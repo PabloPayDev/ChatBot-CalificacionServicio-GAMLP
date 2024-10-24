e2c0098a58e786b29a8f6fc7bfbc288af55f85f8
PabloPayDev <PabloJBecerraVDev@gmail.com>
2024-10-24 09:42:08 -0400

    Pablo - first try MongoDB V2

--- app.py
+++ app.py
@@ -1,16 +1,15 @@
 from flask import Flask, render_template, request, jsonify
-from flask_sqlalchemy import SQLAlchemy
+from flask_pymongo import PyMongo
 from datetime import datetime
 import http.client
 import json
 import logging
 
 app = Flask(__name__)
-
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///metapython.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 logging.basicConfig(level=logging.DEBUG)
-db = SQLAlchemy(app)
+
+app.config['MONGO_URI'] = 'mongodb://localhost:27017/metapython_db'
+mongo = PyMongo(app)
 
 # ======= ======= TEXT TO USE ======= =======
 flow1 = [
@@ -110,31 +109,31 @@ def check_text_in_flow(text, chatbotFlowMessages, index):
     
     return False
 
-
-class Log(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    fecha_y_hora = db.Column(db.DateTime, default=datetime.utcnow)
-    texto = db.Column(db.Text)
-
-with app.app_context():
-    db.create_all()
+def insert_log(texto):
+    log = {
+        'fecha_y_hora': datetime.utcnow(),
+        'texto': texto
+    }
+    mongo.db.logs.insert_one(log)
 
 def sortByDate(register):
     return sorted(register, key=lambda x: x.fecha_y_hora, reverse=True)
 
 @app.route('/')
 def index():
-    registros = Log.query.all()
+    registros = list(mongo.db.logs.find())
     registros_ordenados = sortByDate(registros)
-    return (render_template("index.html", registros = registros_ordenados))
+    return render_template("index.html", registros=registros_ordenados)
 
 mensajes_log = []
 
 def addMessageLog(texto):
     mensajes_log.append(texto)
-    newRegister = Log(texto=json.dumps(texto))
-    db.session.add(newRegister)
-    db.session.commit()
+    newRegister = {
+        'fecha_y_hora': datetime.utcnow(),
+        'texto': json.dumps(texto)
+    }
+    mongo.db.logs.insert_one(newRegister)
 
 TOKEN = "CHATBOTTOKENTEST"
 
